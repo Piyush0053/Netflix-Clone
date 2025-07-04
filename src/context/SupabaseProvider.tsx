0de1bebb8a5e63c0d617605eaf2a8f12dc1ff30c
“[Piyush <118821247+Piyush0053@users.noreply.github.com>
2025-07-04 18:47:40 +0530

    Removed unnecessary files and folders

--- src/context/SupabaseProvider.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface SupabaseProviderProps {
-  children: React.ReactNode;
-}
-
-export const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
-  return <>{children}</>;
-};
